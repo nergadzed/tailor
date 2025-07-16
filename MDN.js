1db10f8476eca8e2ef6c7b75cee5550ea377c54d
Davit Nergadze <nergadzed@gmail.com>
2025-07-16 15:54:08 +0400

    modified:   MDN.css modified:   MDN.js

--- MDN.js
+++ MDN.js
@@ -1,5 +1,8 @@
 window.addEventListener("load", (event) => {
     document.querySelectorAll("details").forEach(element => {
-        element.removeAttribute("open")
+        if (element.getAttribute("aria-current"))
+            void element
+        else
+            element.removeAttribute("open")
     })
 })
